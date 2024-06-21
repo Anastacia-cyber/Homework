8538056c6e0947ded174a74b4c06a181b1916e96
Anastasia <schischenia2011@gmail.com>
2024-06-21 12:06:00 +0300

    homework 10_1 done

--- src/masks.py
+++ src/masks.py
@@ -14,7 +14,6 @@ def get_mask_account(mask_account: str) -> str | None:
         return f"**{mask_account[-4::]}"
 
 
-
 if __name__ == "__main__":
     print(get_mask_card_number("7000792289606361"))
     print(get_mask_account("73654108430135874305"))
